1389af7ed17131321fe8587392e674668ba776c7
Manuel Muñoz Cabrera <Manuel.Munoz.Cabrera@gmail.com>
2018-12-12 11:08:38 -0300

    pruebas de test

--- src/app/modules/login/group.service.spec.ts
+++ src/app/modules/login/group.service.spec.ts
@@ -1,20 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ExpectedConditions } from 'protractor';
+import { GroupService} from './group.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-import { GroupService } from './group.service';
+class HttpClientMock {
+  get = jasmine.createSpy();
+  }
 
-describe('GroupService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ],
-    providers: [
-      GroupService
-    ]
-  }));
+fdescribe('Group services', () => {
+  let service: GroupService;
+  let httpClientMock: HttpClientMock;
 
-  it('should be created', () => {
-    const service: GroupService = TestBed.get(GroupService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{
+        provide: HttpClient ,
+        useClass: HttpClientMock} ,
+        GroupService]
+    });
+    service = TestBed.get(GroupService);
+    httpClientMock = TestBed.get(HttpClient);
+ });
+
+  it('should be created an instance', () => {
+
+       expect(true).toBe(true);
+
+
   });
+
+  it ('test', () => {
+    // expect(true).toBe(false);
+    httpClientMock.get.and.returnValue({
+      toPromise: () => null
+    });
+    const result = service.getGroups();
+    expect( httpClientMock.get).toHaveBeenCalledWith(environment.endpoint.groups);
+    expect(result).toBe(null);
+  });
+
+  it('should be set array of groups and expose a getter', () => {
+      const list =  [1, 2, 3 ];
+      service.setGroups(list);
+      expect(service.getStoredGroups()).toEqual(list);
+    });
 });
--- src/app/modules/login/group.service.ts
+++ src/app/modules/login/group.service.ts
@@ -10,10 +10,20 @@ interface Group {
 @Injectable()
 export class GroupService {
 
+groups = [];
+
   constructor(
     private http: HttpClient
   ) { }
 
+  setGroups(list) {
+    this.groups = list;
+  }
+
+  getStoredGroups() {
+   return this.groups;
+
+  }
   getGroups(): Promise<Group[]> {
     return this.http.get<Group[]>(environment.endpoint.groups)
       .toPromise();
